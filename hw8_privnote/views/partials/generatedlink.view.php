b04262de266655be59f450a5a0e65080ec653e5b
AleksandraHlukhova <a0993715642@gmail.com>
2020-10-19 09:05:55 +0300

    show link

--- hw8_privnote/views/partials/generatedlink.view.php
+++ hw8_privnote/views/partials/generatedlink.view.php
@@ -1,5 +1,7 @@
 <div class="alert alert-success" role="alert">
     <h4>To see your message go
-        <a href="<?php echo $_SERVER['PHP_SELF'] . $_SESSION['link'] ?>">link</a>
+        <a href="<?php echo $_SERVER['PHP_SELF'] . $_SESSION['link'] ?>">
+        <?= $_SERVER['PHP_SELF'] . $_SESSION['link'] ?>
+        </a>
     </h4>
 </div>
